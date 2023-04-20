e79f1701aa4d92419f79e9bf079082365043e003
yoery <81920433+yoery@users.noreply.github.com>
2023-04-20 13:02:05 +0200

    Nav bar

---
+++ navagation.php
@@ -0,0 +1,14 @@
+<section style="height: 0rem">
+      <center>
+        <div>
+          <nav id="nav_bar">
+            <a href="Index.php">Home</a>
+            <a href="login.php">Login</a>
+            <a href="register.php">Aanmelden</a>
+            <a href="alleproducten.php">Alle producten</a>
+            <a href="search.php">Arts</a>
+            <a href="contactpagina.php">Contact</a>
+          </nav>
+        </div>
+      </center>
+    </section>

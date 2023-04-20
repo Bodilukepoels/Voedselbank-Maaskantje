ec05092c41445c604e1ec65aebc17d98b992a8a1
yoery <81920433+yoery@users.noreply.github.com>
2023-04-20 13:14:41 +0200

    navbar changes

--- navigation.php
+++ navigation.php
@@ -1,15 +1,15 @@
-<link rel="stylesheet" href="navigation.css">
+<link rel="stylesheet" type="text/css" media="screen" href="navigation.css" />
 <section style="height: 0rem">
-      <center>
-        <div>
-          <nav id="nav_bar">
-            <a href="Index.php">Home</a>
-            <a href="login.php">Login</a>
-            <a href="register.php">Aanmelden</a>
-            <a href="alleproducten.php">Alle producten</a>
-            <a href="search.php">Arts</a>
-            <a href="contactpagina.php">Contact</a>
-          </nav>
-        </div>
-      </center>
-    </section>
+<center>
+  <div>
+    <nav class="nav_bar">
+      <a href="Index.php">Home</a>
+      <a href="login.php">Login</a>
+      <a href="register.php">Aanmelden</a>
+      <a href="alleproducten.php">Alle producten</a>
+      <a href="search.php">Arts</a>
+      <a href="contactpagina.php">Contact</a>
+    </nav>
+  </div>
+</center>
+</section>

24758503e8ccf72d6729ae608776dfa1e530bf64
Bodilukepoels <112855865+Bodilukepoels@users.noreply.github.com>
2023-04-24 09:32:29 +0200

    nav veranderd

--- navigation.php
+++ navigation.php
@@ -7,7 +7,6 @@
       <a href="login.php">Login</a>
       <a href="register.php">Aanmelden</a>
       <a href="alleproducten.php">Alle producten</a>
-      <a href="search.php">Arts</a>
       <a href="contactpagina.php">Contact</a>
     </nav>
   </div>

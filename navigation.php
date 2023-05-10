4eb55d83be9d6c3343ff7261f917613a1d2331c3
ik <bruh@lmbao.lol>
2023-05-10 13:23:53 +0200

    sus

--- navigation.php
+++ navigation.php
@@ -1,10 +1,7 @@
-
-<!DOCTYPE html>
-<html>
-<head>
-  <link rel="stylesheet" type="text/css" media="screen" href="navigation.css" />
-</head>
-<body>
+  <?php
+  session_start();
+  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+  ?>
   <section>
     <center>
       <div>
@@ -13,22 +10,17 @@
           <div onclick="location.href='alleproducten.php';"><a href="alleproducten.php">Producten</a></div>
           <div onclick="location.href='contactpagina.php';"><a href="contactpagina.php">Contact</a></div>
           <div onclick="location.href='winkelmand.php';"><a href="winkelmand.php">Winkelmand</a></div>
-          <?php
-          session_start();
-          if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-              echo "<div class=\"dropdown\">";
-              echo "<div style= 'color: black;'>Welkom, " . $_SESSION['user_name'] . "</div>";
-              echo "<div class=\"dropdown-content\">";
-              echo "<a href=\"logout.php\">Logout</a>";
-              echo "</div>";
-              echo "</div>";
-          } else {
-              echo "<div onclick=\"location.href='login.php';\"><a href=\"login.php\">Login</a></div>";
-          }
-          ?>
+          
+          <div class="dropdown">
+            <div style= 'color: black;'>Welkom, <?php echo $_SESSION['user_name']; ?></div>
+            <div class="dropdown-content">
+              <a href="logout.php">Logout</a>
+            </div>
+          </div>
         </nav>
       </div>
     </center>
   </section>
-</body>
-</html>
+<?php
+}
+?>

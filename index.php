6c96bd20255e515ba58eaf7bb41f37acea0c6f2e
ik <bruh@lmbao.lol>
2023-05-25 12:32:11 +0200

    ye

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
-    <h1>test</h1>
     <?php
     include 'navigation.php';
     echo "<br><div style='text-align: center;'><h1 style='color: black;'>Voedselbank Maaskant</h1><br>";

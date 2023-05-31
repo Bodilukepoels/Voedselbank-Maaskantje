6f5c8924e467e0d1211c30e3b7a9c5fdbba54df3
yoery <81920433+yoery@users.noreply.github.com>
2023-05-31 14:45:45 +0200

    ik ben goed in coderen

--- leveranciersoverzicht.php
+++ leveranciersoverzicht.php
@@ -1,59 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Overzicht producten</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="bg-light">
-    <?php
-        include "leverancierstoevoegen.php";
-        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    ?>
-    <br>
-    <CENTER><h1 style="color: black;">Overzicht leveranciers</h1>
-    <div class="table-responsive mt-4">
-        <table class="table table-striped">
-        <tr>
-            <th>ID</th>
-            <th>Naam</th>
-            <th>Beschrijving</th>
-            <th>Voorraad</th>
-            <th>EAN Nummer</th>
-        </tr>
-        </div>
-        </CENTER>
-        <?php
-        include 'config.php';
-
-        try {
-            $sql = "SELECT * FROM leveranciers";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $products = $stmt->fetchAll();
-
-            foreach ($products as $product) {
-                echo "<tr>";
-                echo "<td>" . $product['id'] . "</td>";
-                echo "<td>" . $product['naam'] . "</td>";
-                echo "<td>" . $product['beschrijving'] . "</td>";
-                echo "<td>" . $product['voorraad'] . "</td>";
-                echo "<td>" . $product['EAN-Nummer'] . "</td>";
-                echo "</tr>";
-            }
-        } catch (PDOException $e) {
-            echo "<div class='alert alert-danger'>" . $e->getMessage() . "</div>";
+    <style>
+        body {
+            padding: 20px;
         }
+
+        th {
+            color: black;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+ 
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     ?>
-        ?>
-    </table>
+        <div class="container">
+            <h1 class="text-center mt-5">Overzicht leveranciers</h1>
+            <div class="table-responsive mt-4">
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>Naam</th>
+                            <th>E-mail</th>
+                            <th>Telefoonnummer</th>
+                            <th>Bezorgingsdatum</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        include 'config.php';
+
+                        try {
+                            $sql = "SELECT * FROM leveranciers";
+                            $stmt = $conn->prepare($sql);
+                            $stmt->execute();
+                            $leveranciers = $stmt->fetchAll();
+
+                            foreach ($leveranciers as $leverancier) {
+                                echo "<tr>";
+                                echo "<td>" . $leverancier['id'] . "</td>";
+                                echo "<td>" . $leverancier['naam'] . "</td>";
+                                echo "<td>" . $leverancier['mail'] . "</td>";
+                                echo "<td>" . $leverancier['telefoonnummer'] . "</td>";
+                                echo "<td>" . $leverancier['bezorgingsdatum'] . "</td>";
+                                echo "</tr>";
+                            }
+                        } catch (PDOException $e) {
+                            echo "<div class='alert alert-danger'>" . $e->getMessage() . "</div>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    <?php
+    } else {
+        header("Location: leverancierstoevoegen.php");
+    }
+    ?>
 </body>
+
 </html>
-  <?php
-  }
-  else {
-    header("Location: index.php");
-  }
-  ?>
-    
--- leverancierstoevoegen.php
+++ leverancierstoevoegen.php
@@ -1,81 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Leverancier Management</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <style>
+        body {
+            padding: 20px;
+        }
+
+        .container {
+            max-width: 800px;
+        }
+
+        h1, h2 {
+            margin-top: 30px;
+            color: black;
+        }
+    </style>
 </head>
+
 <body>
     <?php
-        include "navigation.php";
+    include "navigation.php";
     ?>
-    <h1>Leverancier toevoegen</h1>
-    <h2>Add a new Leverancier</h2>
-    <form action="leveranciers.php" method="POST">
-        <label for="naam">Name:</label>
-        <input type="text" id="naam" name="naam" required><br>
-        <label for="beschrijving">Beschrijving:</label>
-        <input type="text" id="beschrijving" name="beschrijving" required><br>
-        <label for="Telefoonnummer">Telefoonnummer:</label>
-        <input type="number" id="Telefoonnummer" name="Telefoonnummer" required><br>
-        <label for="Mail">Mail:</label>
-        <input type="email" id="Mail" name="Mail" required><br>
-        <label for="postcode">Postcode:</label>
-        <input type="text" id="postcode" name="postcode" required><br>
-        <input type="submit" value="Add leverancier">
-    </form>
+    <div class="container">
+        <h1>Leverancier toevoegen</h1>
+        <h2>Add a new Leverancier</h2>
+        <form action="leveranciers.php" method="POST">
+            <div class="form-group">
+                <label for="naam">Name:</label>
+                <input type="text" class="form-control" id="naam" name="naam" required>
+            </div>
+            <div class="form-group">
+                <label for="beschrijving">Beschrijving:</label>
+                <input type="text" class="form-control" id="beschrijving" name="beschrijving" required>
+            </div>
+            <div class="form-group">
+                <label for="Telefoonnummer">Telefoonnummer:</label>
+                <input type="number" class="form-control" id="Telefoonnummer" name="Telefoonnummer" required>
+            </div>
+            <div class="form-group">
+                <label for="Mail">Mail:</label>
+                <input type="email" class="form-control" id="Mail" name="Mail" required>
+            </div>
+            <div class="form-group">
+                <label for="postcode">Postcode:</label>
+                <input type="text" class="form-control" id="postcode" name="postcode" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Add leverancier</button>
+        </form>
 
-    <h2>Bestaande leveranciers</h2>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Naam</th>
-            <th>Beschrijving</th>
-            <th>Telefoonnummer</th>
-            <th>Mail</th>
-            <th>Postcode</th>
-        </tr>
-        <?php
-        include 'config.php';
+        <h2>Bestaande leveranciers</h2>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>Naam</th>
+                    <th>Beschrijving</th>
+                    <th>Telefoonnummer</th>
+                    <th>Mail</th>
+                    <th>Postcode</th>
+                    <th>Edit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include 'config.php';
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $naam = $_POST['naam'];
-            $beschrijving = $_POST['beschrijving'];
-            $Telefoonnummer = $_POST['Telefoonnummer'];
-            $Mail = $_POST['Mail'];
-            $postcode = $_POST['postcode']; // Define this variable
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                    $naam = $_POST['naam'];
+                    $beschrijving = $_POST['beschrijving'];
+                    $Telefoonnummer = $_POST['Telefoonnummer'];
+                    $Mail = $_POST['Mail'];
+                    $postcode = $_POST['postcode'];
 
-            try {
-                $sql = "INSERT INTO leveranciers (naam, beschrijving, Telefoonnummer, `Mail`, postcode) VALUES (?, ?, ?, ?, ?)";
-                $stmt = $conn->prepare($sql);
-                $stmt->execute([$naam, $beschrijving, $Telefoonnummer, $Mail, $postcode]);
-            } catch (PDOException $e) {
-                echo "Error: " . $e->getMessage();
-            }
-        }
+                    try {
+                        $sql = "INSERT INTO leveranciers (naam, beschrijving, Telefoonnummer, `Mail`, postcode) VALUES (?, ?, ?, ?, ?)";
+                        $stmt = $conn->prepare($sql);
+                        $stmt->execute([$naam, $beschrijving, $Telefoonnummer, $Mail, $postcode]);
+                    } catch (PDOException $e) {
+                        echo "Error: " . $e->getMessage();
+                    }
+                }
 
+                try {
+                    $sql = "SELECT * FROM leveranciers";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute();
+                    $leveranciers = $stmt->fetchAll();
 
-        try {
-            $sql = "SELECT * FROM leveranciers";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $leveranciers = $stmt->fetchAll();
-
-            foreach ($leveranciers as $leverancier) {
-                echo "<tr>";
-                echo "<td>" . $leverancier['id'] . "</td>";
-                echo "<td>" . $leverancier['naam'] . "</td>";
-                echo "<td>" . $leverancier['Telefoonnummer'] . "</td>";
-                echo "<td>" . $leverancier['Mail'] . "</td>";
-                echo "<td>" . $leverancier['Postcode'] . "</td>";
-                echo "<td><a href='edit_leverancier.php?id=" . $leverancier['id'] . "'>Edit</a></td>";
-                echo "</tr>";
-            }
-        } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
-        }
-        ?>
-    </table>
+                    foreach ($leveranciers as $leverancier) {
+                        echo "<tr>";
+                        echo "<td>" . $leverancier['id'] . "</td>";
+                        echo "<td>" . $leverancier['naam'] . "</td>";
+                        echo "<td>" . $leverancier['beschrijving'] . "</td>";
+                        echo "<td>" . $leverancier['Telefoonnummer'] . "</td>";
+                        echo "<td>" . $leverancier['Mail'] . "</td>";
+                        echo "<td>" . $leverancier['postcode'] . "</td>";
+                        echo "<td><a href='edit_leverancier.php?id=" . $leverancier['id'] . "' class='btn btn-primary btn-sm'>Edit</a></td>";
+                        echo "</tr>";
+                    }
+                } catch (PDOException $e) {
+                    echo "Error: " . $e->getMessage();
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
+
 </html>

677d964f5432452912b27a6d3a4670deb44670ca
yoery <81920433+yoery@users.noreply.github.com>
2023-05-31 14:16:22 +0200

    Update leveranciersoverzicht.php

--- leveranciersoverzicht.php
+++ leveranciersoverzicht.php
@@ -3,40 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leveranciers Details</title>
-    <link rel="stylesheet" href="styles.css">
-    <script>
-        function addToCart(id) {
-            // Define your add to cart functionality here
-        }
-    </script>
+    <title>Overzicht producten</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
+<body class="bg-light">
     <?php
-    include 'navigation.php';
-
-    if (isset($_GET['id'])) {
-        require_once 'config.php';
-
-        $sql = "SELECT * FROM leveranciers WHERE id = :id"; // Check your table name
-        $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':id', $_GET['id']);
-        $stmt->execute();
-
-        if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo "<div class='leverancier-details'>
-                    <img src='path/to/image/{$row['image']}' alt='{$row['naam']}' /> <!-- Correct the path -->
-                    <h2>{$row['naam']}</h2>
-                    <p>{$row['beschrijving']}</p>
-                    <button type='button' onclick='addToCart({$row['id']})'>Add to cart</button>
-                  </div>";
-        } else {
-            echo "<p>leverancier not found.</p>";
-        }
-    } else {
-        echo "<p>Invalid leverancier ID.</p>";
-    }
+        include "leverancierstoevoegen.php";
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     ?>
+    <br>
+    <CENTER><h1 style="color: black;">Overzicht leveranciers</h1>
+    <div class="table-responsive mt-4">
+        <table class="table table-striped">
+        <tr>
+            <th>ID</th>
+            <th>Naam</th>
+            <th>Beschrijving</th>
+            <th>Voorraad</th>
+            <th>EAN Nummer</th>
+        </tr>
+        </div>
+        </CENTER>
+        <?php
+        include 'config.php';
 
+        try {
+            $sql = "SELECT * FROM leveranciers";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $products = $stmt->fetchAll();
+
+            foreach ($products as $product) {
+                echo "<tr>";
+                echo "<td>" . $product['id'] . "</td>";
+                echo "<td>" . $product['naam'] . "</td>";
+                echo "<td>" . $product['beschrijving'] . "</td>";
+                echo "<td>" . $product['voorraad'] . "</td>";
+                echo "<td>" . $product['EAN-Nummer'] . "</td>";
+                echo "</tr>";
+            }
+        } catch (PDOException $e) {
+            echo "<div class='alert alert-danger'>" . $e->getMessage() . "</div>";
+        }
+    ?>
+        ?>
+    </table>
 </body>
 </html>
+  <?php
+  }
+  else {
+    header("Location: index.php");
+  }
+  ?>
+    

edc23cf96dd9f9384e4dfd086eb62e5f96c34df7
Bodilukepoels <112855865+Bodilukepoels@users.noreply.github.com>
2023-06-07 22:47:49 +0200

    Update indexcontents.php

--- indexcontents.php
+++ indexcontents.php
@@ -4,12 +4,8 @@
     <a href="logout.php"><h1>Logout</h1></a>
 </div>
 <br><br>
-
-<<<<<<< Updated upstream
 <?php
 include "config.php";
-
-// Retrieve the current food package
 try {
     $stmt = $conn->prepare("SELECT * FROM voedselpakket ORDER BY id DESC LIMIT 1");
     $stmt->execute();
@@ -33,17 +29,3 @@ try {
     echo '<p>Fout bij het ophalen van het voedselpakket: ' . $e->getMessage() . '</p>';
 }
 ?>
-=======
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-	<div style="color: black; box-shadow: 2px 2px 2px 2px; padding: 15px;" class="container">
-	<h2>Welkom, <?php echo $_SESSION['user_name']; ?>!</h2>  
-	<a href="logout.php"><h1>Logout</h1></a>
-	</div>
-	<br><br>
-<a href="food_package_details.php">
-    <div style="text-align: center;">
-        <img src="https://cdn.pixabay.com/photo/2012/04/24/16/17/box-40302_960_720.png" width="200px" style="cursor: pointer;">
-        <p>Klik hier om de details van het voedselpakket te bekijken</p>
-    </div>
-</a>
->>>>>>> Stashed changes

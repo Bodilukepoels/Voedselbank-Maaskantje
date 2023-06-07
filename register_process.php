b2e8d5b779c03c6558eeb426fbadba1d40ea173a
Bodilukepoels <112855865+Bodilukepoels@users.noreply.github.com>
2023-06-07 22:58:14 +0200

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -6,7 +6,7 @@ if (isset($_POST['email'], $_POST['password'], $_POST['confirm_password'])) {
         $password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
         print_r($_POST);
         try {
-            $sql = "INSERT INTO user (Email, Wachtwoord, Naam, Telefoonnummer, IsAdmin) VALUES (:Email, :Wachtwoord, :Naam, :Telefoonnummer, 0)";
+            $sql = "INSERT INTO user (Email, Wachtwoord, Naam, Telefoonnummer) VALUES (:Email, :Wachtwoord, :Naam, :Telefoonnummer)";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':Email', $_POST['email']);
             $stmt->bindParam(':Wachtwoord', $password_hash);

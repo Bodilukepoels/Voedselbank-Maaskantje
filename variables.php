eb633c6c69858c5a0d63d1a30f70c2b398781f6b
TufanBudus <bruh@lmbao.lol>
2023-06-08 14:37:33 +0200

    :raised eye brow:

---
+++ variables.php
@@ -0,0 +1,14 @@
+<?php
+$categories = array(
+    '            ',
+    'Aardappelen, Groente, Fruit',
+    'Kaas, Vleeswaren',
+    'Zuivel, Plantaardig en eieren',
+    'Bakkerij en Banket',
+    'Frisdrank, Sappen, Kofie en Thee',
+    'Pasta, Rijst en wereldkeuken',
+    'Soepen, Sauzen, Kruiden en Olie',
+    'Snoep, Koek, Chips en Chocolade',
+    'Baby, Verzorging, Hygiene',
+);
+?>
